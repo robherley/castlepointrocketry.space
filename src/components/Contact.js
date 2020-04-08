f25c7c736979e1e586daa67ff6992dff95736cd7
Ben Iofel <ben@iofel.me>
2020-04-07 20:24:36 -0400

    Update contact emails

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,12 +12,12 @@ const Contact = () => {
             Are you a member of the aerospace industry? An experienced academic?
             A passionate rocketry enthusiast? Contact us to learn more or book
             your launch at{" "}
-            <a href="mailto:contact@cprocketry.com">contact@cprocketry.com</a>.
+            <a href="mailto:team@hudsonspace.systems">team@hudsonspace.systems</a>.
           </p>
           <br />
           <p>
             Interested in our mission and joining our team? Email{" "}
-            <a href="mailto:careers@cprocketry.com">careers@cprocketry.com</a>.
+            <a href="mailto:jobs@hudsonspace.systems">jobs@hudsonspace.systems</a>.
           </p>
         </section>
       </div>
