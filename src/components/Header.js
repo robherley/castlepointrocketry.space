a792624eebc87dcbd1f80010b640ddf52a756791
Ben Iofel <ben@iofel.me>
2020-09-08 21:08:15 -0400

    Add investment banner

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Link, navigate } from "gatsby"
+import { useLocation } from '@reach/router'
 
 import { useScrollHeight } from "../hooks/"
 import Logo from "../images/HSSwhite.svg"
@@ -38,17 +39,41 @@ const links = [
 const Header = () => {
   const [isOpen, setOpen] = useState(false)
   const scrollHeight = useScrollHeight()
+  const location = useLocation()
+
   return (
-    <header className={scrollHeight > 60 ? "floating" : ""}>
-      <div className="container">
-        <div
-          className="logo-container"
-          onClick={() => navigate("/")}
-          tabIndex="0"
-        >
-          <img className="logo-icon" src={Logo} alt="logo" />
+    <>
+      { location.pathname === '/' &&
+        <h2 className="banner">Become a part of our company by <a target="_blank" href="https://invest.microventures.com/upcoming/hudson-space-systems/?referral_code=HSSWEB090820">investing</a> in HSS on MicroVentures</h2> }
+      <header className={scrollHeight > 60 ? "floating" : ""}>
+        <div className="container">
+          <div
+            className="logo-container"
+            onClick={() => navigate("/")}
+            tabIndex="0"
+          >
+            <img className="logo-icon" src={Logo} alt="logo" />
+          </div>
+          <div className="links">
+            {links.map(e => (
+              <Link
+                {...e}
+                key={e.children.toString()}
+                onClick={() => setOpen(false)}
+              />
+            ))}
+          </div>
+          <div className="burger" onClick={() => setOpen(!isOpen)} tabIndex="0">
+            <div className="meat" />
+          </div>
         </div>
-        <div className="links">
+        <div
+          className="mobile-nav"
+          style={{
+            pointerEvents: isOpen ? "auto" : "none",
+            opacity: isOpen ? "1" : "0",
+          }}
+        >
           {links.map(e => (
             <Link
               {...e}
@@ -57,26 +82,8 @@ const Header = () => {
             />
           ))}
         </div>
-        <div className="burger" onClick={() => setOpen(!isOpen)} tabIndex="0">
-          <div className="meat" />
-        </div>
-      </div>
-      <div
-        className="mobile-nav"
-        style={{
-          pointerEvents: isOpen ? "auto" : "none",
-          opacity: isOpen ? "1" : "0",
-        }}
-      >
-        {links.map(e => (
-          <Link
-            {...e}
-            key={e.children.toString()}
-            onClick={() => setOpen(false)}
-          />
-        ))}
-      </div>
-    </header>
+      </header>
+    </>
   )
 }
 
