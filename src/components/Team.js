f53382bb5c1c8fa83587a62aeae3d8b85a9f3a3e
Ben Iofel <ben@iofel.me>
2020-09-09 19:10:18 -0400

    Add linkedin links

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,35 +1,43 @@
 import React from 'react'
+import { Linkedin, GitHub } from 'react-feather'
 
 const team = [
   {
     name: "Nathan Tahbaz",
     role: "Team Lead, Design and Fabrication",
     photo: require("../images/team/nathan-min.jpg"),
+    linkedin: 'nathan-tahbaz',
   },
   {
     name: "Monica Traupmann",
     role: "Propulsion Specialist, Materials",
     photo: require("../images/team/monica-min.jpg"),
+    linkedin: 'monica-traupmann',
   },
   {
     name: "Ben Iofel",
-    role: "Lead Programmer",
+    role: "Avionics Specialist, Lead Programmer",
     photo: require("../images/team/ben-min.jpg"),
+    linkedin: 'beniofel',
+    github: 'benwaffle'
   },
   {
     name: "William Skwirut",
     role: "Aerodynamic Design and Machining Specialist",
     photo: require("../images/team/will-min.jpg"),
+    linkedin: 'william-skwirut-853b43128',
   },
   {
     name: "Dakota Van Deursen",
     role: "Propulsion Specialist, Materials, and Media",
     photo: require("../images/team/dakota-min.jpg"),
+    linkedin: 'dvandeursen',
   },
   {
     name: "Nicholas Yarbrough",
     role: "Electrical Engineering and Embedded System Design",
     photo: require("../images/team/cole-min.jpg"),
+    linkedin: 'cole-yarbrough-2a39551b2',
   },
 ]
 
@@ -38,11 +46,13 @@ const advisors = [
     name: 'Igor Alexandrov',
     role: 'Senior Advisor',
     photo: require('../images/team/igor.jpg'),
+    linkedin: 'igor-alexandrov-033123',
   },
   {
     name: 'Khurram Nasir Gore',
     role: 'Senior Advisor',
     photo: require('../images/team/khurram.jpg'),
+    linkedin: 'kngore',
   }
 ]
 
@@ -58,6 +68,15 @@ const Team = () => {
                 <img src={e.photo} alt={e.name} />
                 <p className="bold">{e.name}</p>
                 <p style={{ marginTop: "0.2rem" }}>{e.role}</p>
+                <div className="icons">
+                  <a target="_blank" href={`https://linkedin.com/in/${e.linkedin}`}>
+                    <Linkedin className="linkedin" />
+                  </a>
+                  { e.github &&
+                    <a target="_blank" href={`https://github.com/${e.github}`}>
+                      <GitHub />
+                    </a> }
+                </div>
               </div>
             ))}
           </div>
@@ -70,6 +89,15 @@ const Team = () => {
                 <img src={e.photo} alt={e.name} />
                 <p className="bold">{e.name}</p>
                 <p style={{ marginTop: "0.2rem" }}>{e.role}</p>
+                <div className="icons">
+                  <a target="_blank" href={`https://linkedin.com/in/${e.linkedin}`}>
+                    <Linkedin className="linkedin" />
+                  </a>
+                  { e.github &&
+                    <a target="_blank" href={`https://github.com/${e.github}`}>
+                      <GitHub />
+                    </a> }
+                </div>
               </div>
             ))}
           </div>
