d88254d1f8fac1bf61cd599d5afe432c6833e23e
Ben Iofel <ben@iofel.me>
2020-10-25 23:00:42 -0400

    Components (#7)

---
+++ src/components/Components.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+export const Heading = styled.h1`
+  font-size: 4rem;
+`
+
+const SectionRoot = styled.section`
+  padding: 2rem 2rem;
+
+  :nth-child(even) {
+    background-color: var(--bg-light);
+    color: var(--text-dark);
+  }
+
+  :nth-child(odd) {
+    background-color: var(--bg-dark);
+    color: var(--text-light);
+  }
+
+  ${Heading} {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+`
+
+const SectionContent = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  @media (min-width: 972px) {
+    width: 60%;
+  }
+}
+`
+
+export function Section({ children, className }) {
+  return <SectionRoot className={className}>
+    <SectionContent>
+      {children}
+    </SectionContent>
+  </SectionRoot>
+}
+
+export const Paragraph = styled.p`
+  line-height: 1.4rem;
+
+  & + p {
+    margin-top: 1rem;
+  }
+`
--- src/sections/About.js
+++ src/sections/About.js
@@ -1,50 +1,42 @@
 import React from 'react'
-import styled from "@emotion/styled"
+import styled from '@emotion/styled'
+import { Section, Heading, Paragraph } from '../components/Components'
 
 const Container = styled.section`
   margin: 2rem 2rem;
 `
 
+const CenterVideo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
 const Video = styled.iframe`
   --width: calc(100vw - 4rem);
   @media (min-width: 972px) {
-    --width: 60vw;
+    --width: calc(100vw * .6);
   }
 
   width: var(--width);
   height: calc(var(--width) / 1.7777777);
   display: block;
-  margin: 0 auto;
-`
-
-const Text = styled.article`
-  margin: 2rem auto;
-
-  @media (min-width: 972px) {
-    width: 60%;
-  }
-`
-
-const Paragraph = styled.p`
-  font-size: 1.2rem;
-  color: white;
-  line-height: 1.3;
   margin-bottom: 2rem;
 `
 
 export default function About() {
-  return <Container>
-    <Video src="https://www.youtube.com/embed/ni1DfT1ew8Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></Video>
-    <Text>
-      <Paragraph>
-        What if science could move faster? Hudson Space Systems is dedicated to accelerating the speed of scientific advancement. We believe that the current paradigm of research is artificially limiting innovation. By providing fast and affordable access to microgravity, Hudson Space Systems empowers a broad spectrum of customers to make breakthroughs faster. We will achieve this with our Microgravity as a Service business model, coupled with our innovative, fully-reusable launch vehicle.
-      </Paragraph>
-      <Paragraph>
-        From petrochemical R&D to university labs, STEM research can benefit from access to microgravity. The launch providers currently servicing this market are already operating at capacity, with extremely long lead times and high prices. Hudson Space Systems plans to leverage our launch platform to expand this market by shattering price barriers, slashing lead times, and providing end-to-end user support.
-      </Paragraph>
-      <Paragraph>
-        We're developing the next generation of reusable space launch vehicles, featuring our patent-pending engine technology and high-efficiency designs, to accelerate the next wave of innovation in space.
-      </Paragraph>
-    </Text>
-  </Container>
+  return <Section>
+           <CenterVideo>
+             <Video src="https://www.youtube.com/embed/ni1DfT1ew8Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></Video>
+           </CenterVideo>
+           <Paragraph>
+             What if science could move faster? Hudson Space Systems is dedicated to accelerating the speed of scientific advancement. We believe that the current paradigm of research is artificially limiting innovation. By providing fast and affordable access to microgravity, Hudson Space Systems empowers a broad spectrum of customers to make breakthroughs faster. We will achieve this with our Microgravity as a Service business model, coupled with our innovative, fully-reusable launch vehicle.
+           </Paragraph>
+           <Paragraph>
+             From petrochemical R&D to university labs, STEM research can benefit from access to microgravity. The launch providers currently servicing this market are already operating at capacity, with extremely long lead times and high prices. Hudson Space Systems plans to leverage our launch platform to expand this market by shattering price barriers, slashing lead times, and providing end-to-end user support.
+           </Paragraph>
+           <Paragraph>
+             We're developing the next generation of reusable space launch vehicles, featuring our patent-pending engine technology and high-efficiency designs, to accelerate the next wave of innovation in space.
+           </Paragraph>
+         </Section>
 }
