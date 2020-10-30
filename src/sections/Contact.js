a5f847a1ef0f9fd56cbc84edc120792f4a6bd2a9
Ben Iofel <ben@iofel.me>
2020-10-30 02:07:49 -0400

    Update contact section

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,5 +1,23 @@
 import React from "react"
+import styled from '@emotion/styled'
 import { Section, Heading } from '../components/Components'
+
+const ContactMethod = styled.div`
+  margin-top: 1rem;
+  font-size: 1.5rem;
+`
+
+const ContactTitle = styled.span`
+  color: var(--cpr-blue);
+`
+
+const ContactDetail = styled.span`
+  float: right;
+
+  a {
+    text-decoration: underline; // TODO: make links underlined by default
+  }
+`
 
 const Contact = () => {
   return (
@@ -7,22 +25,22 @@ const Contact = () => {
       <div className="container">
         <Heading>Contact Us</Heading>
         <section>
-          <p>
-            Are you a member of the aerospace industry? An experienced academic?
-            A passionate rocketry enthusiast? Contact us to learn more at{" "}
-            <a href="mailto:contact@hudsonspace.systems">contact@hudsonspace.systems</a>
-            {" "}or call us at{" "}
-            <a href="tel:12012687887">(201) 268-7887</a>
-          </p>
-          <br />
-          <p>
-            Interested in booking a launch? Fill out <a href="https://airtable.com/shrt3DGgqmZ6KZwcC">an inquiry form</a>.
-          </p>
-          <br />
-          <p>
-            Interested in our mission and joining our team? Email{" "}
-            <a href="mailto:jobs@hudsonspace.systems">jobs@hudsonspace.systems</a>.
-          </p>
+          <ContactMethod>
+            <ContactTitle>Send us a message</ContactTitle>
+            <ContactDetail><a href="mailto:contact@hudsonspace.systems">contact@hudsonspace.systems</a></ContactDetail>
+          </ContactMethod>
+          <ContactMethod>
+            <ContactTitle>Give us a call</ContactTitle>
+            <ContactDetail><a href="tel:12012687887">(201) 268-7887</a></ContactDetail>
+          </ContactMethod>
+          <ContactMethod>
+            <ContactTitle>Inquire into launch services</ContactTitle>
+            <ContactDetail>Fill out our <a href="https://airtable.com/shrt3DGgqmZ6KZwcC">form</a></ContactDetail>
+          </ContactMethod>
+          <ContactMethod>
+            <ContactTitle>See our origin story</ContactTitle>
+            <ContactDetail><a href="https://castlepointrocketry.space/news/">castlepointrocketry.space</a></ContactDetail>
+          </ContactMethod>
         </section>
       </div>
     </Section>
