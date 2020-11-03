64ca8547ec1b10529d1caa83b70c8c7900366975
Ben Iofel <iofelben@gmail.com>
2020-11-03 00:07:11 -0500

    sizing

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -2,32 +2,56 @@ import React from "react"
 import styled from '@emotion/styled'
 import { Section, Heading } from '../components/Components'
 
+const MobileSize = `564px`
+
 const Container = styled.div`
   margin: 0 auto;
 
-  @media (min-width: 972px) {
-    width: 60%;
-    min-width: 35rem;
+  @media (min-width: ${MobileSize}) {
+    width: 70%;
+    min-width: 32rem;
+  }
+`
+
+const Body = styled.section`
+  width: 100%;
+  margin: 0 auto;
+
+  @media (min-width: 377px) { // not so small phones
+    width: 80%;
+  }
+  @media (min-width: ${MobileSize}) {
+    width: 100%;
   }
 `
 
 const ContactMethod = styled.div`
   margin-top: 1rem;
   font-size: 1.0rem;
+  display: flex;
+  flex-direction: column;
 
-  @media (min-width: 972px) {
+  @media (min-width: ${MobileSize}) {
     font-size: 1.3rem;
+    flex-direction: row;
   }
 `
 
 const ContactTitle = styled.span`
+  flex-grow: 1;
   color: var(--cpr-red);
   font-weight: bold;
   margin-right: 1rem;
 `
 
 const ContactDetail = styled.span`
-  float: right;
+  flex-grow: 1;
+  text-align: left;
+
+  @media (min-width: ${MobileSize}) {
+    text-align: right;
+  }
+
   a {
     text-decoration: underline; // TODO: make links underlined by default
   }
@@ -38,7 +62,7 @@ const Contact = () => {
     <Section className="contact" id="contact">
       <Container>
         <Heading>Contact Us</Heading>
-        <section>
+        <Body>
           <ContactMethod>
             <ContactTitle>Send us a message</ContactTitle>
             <ContactDetail><a href="mailto:contact@hudsonspace.systems">contact@hudsonspace.systems</a></ContactDetail>
@@ -55,7 +79,7 @@ const Contact = () => {
             <ContactTitle>See our origin story</ContactTitle>
             <ContactDetail><a href="https://castlepointrocketry.space/news/">castlepointrocketry.space</a></ContactDetail>
           </ContactMethod>
-        </section>
+        </Body>
       </Container>
     </Section>
   )
