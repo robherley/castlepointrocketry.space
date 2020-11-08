f7c721acdce21f81fdf005fa2bac0bd79cab70bc
Ben Iofel <iofelben@gmail.com>
2020-11-08 16:20:27 -0500

    Simplify

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -1,40 +1,25 @@
 import React from "react"
 import styled from '@emotion/styled'
 import { Section, Heading } from '../components/Components'
-
-const MobileSize = `564px`
+import mq from '../components/Breakpoints'
 
 const Container = styled.div`
   margin: 0 auto;
 
-  @media (min-width: ${MobileSize}) {
-    width: 70%;
-    min-width: 32rem;
-  }
-`
-
-const Body = styled.section`
-  width: 100%;
-  margin: 0 auto;
-
-  @media (min-width: 377px) { // not so small phones
-    width: 80%;
-  }
-  @media (min-width: ${MobileSize}) {
-    width: 100%;
-  }
+  ${mq({
+    width: ['auto', '70%'],
+    minWidth: ['auto', '32rem'],
+  })}
 `
 
 const ContactMethod = styled.div`
   margin-top: 1rem;
-  font-size: 1.0rem;
   display: flex;
-  flex-direction: column;
 
-  @media (min-width: ${MobileSize}) {
-    font-size: 1.3rem;
-    flex-direction: row;
-  }
+  ${mq({
+    fontSize: ['1rem', '1.3rem'],
+    flexDirection: ['column', 'row'],
+  })}
 `
 
 const ContactTitle = styled.span`
@@ -46,11 +31,10 @@ const ContactTitle = styled.span`
 
 const ContactDetail = styled.span`
   flex-grow: 1;
-  text-align: left;
 
-  @media (min-width: ${MobileSize}) {
-    text-align: right;
-  }
+  ${mq({
+    textAlign: ['left', 'right'],
+  })}
 
   a {
     text-decoration: underline; // TODO: make links underlined by default
@@ -62,7 +46,7 @@ const Contact = () => {
     <Section className="contact" id="contact">
       <Container>
         <Heading>Contact Us</Heading>
-        <Body>
+        <section>
           <ContactMethod>
             <ContactTitle>Send us a message</ContactTitle>
             <ContactDetail><a href="mailto:contact@hudsonspace.systems">contact@hudsonspace.systems</a></ContactDetail>
@@ -73,13 +57,13 @@ const Contact = () => {
           </ContactMethod>
           <ContactMethod>
             <ContactTitle>Inquire into launch services</ContactTitle>
-            <ContactDetail>Fill out our <a href="https://airtable.com/shrt3DGgqmZ6KZwcC">form</a></ContactDetail>
+            <ContactDetail>Fill out our <a href="https://airtable.com/shrt3DGgqmZ6KZwcC" target="_blank" rel="noreferrer">form</a></ContactDetail>
           </ContactMethod>
           <ContactMethod>
             <ContactTitle>See our origin story</ContactTitle>
             <ContactDetail><a href="https://castlepointrocketry.space/news/">castlepointrocketry.space</a></ContactDetail>
           </ContactMethod>
-        </Body>
+        </section>
       </Container>
     </Section>
   )
