678736b7324e7b601461ecc9944587c3324a04e7
Ben Iofel <iofelben@gmail.com>
2020-11-15 17:01:08 -0500

    Comparison table

--- src/sections/Comparison.js
+++ src/sections/Comparison.js
@@ -6,6 +6,7 @@ import EiffelTower from '../images/eiffeltower.png'
 import ISS from '../images/iss.png'
 import Rocket from '../images/rocket_white.png'
 import mq from '../components/Breakpoints'
+import { Check, X } from 'react-feather'
 
 const Container = styled.div`
   display: flex;
@@ -53,6 +54,78 @@ const Image = styled.div`
   }
 `
 
+const Table = styled.table`
+  thead {
+    font-weight: bold;
+
+    td {
+      background: #393939;
+
+      &:first-child {
+        background: none;
+      }
+
+      &:nth-child(3) {
+        border-top: 1px solid var(--cpr-yellow);
+        border-left: 1px solid var(--cpr-yellow);
+        border-right: 1px solid var(--cpr-yellow);
+      }
+    }
+  }
+
+  tbody {
+    background: #262626;
+
+    tr + tr {
+      border-top: 1px solid #393939;
+    }
+
+    tr {
+      td:nth-child(3) {
+        border-left: 1px solid var(--cpr-yellow);
+        border-right: 1px solid var(--cpr-yellow);
+      }
+
+      &:last-child {
+        td:nth-child(3) {
+          border-bottom: 1px solid var(--cpr-yellow);
+        }
+      }
+    }
+  }
+
+  td {
+    padding: .5rem .7rem;
+  }
+`
+
+const Circle = styled.div`
+  width: 1rem;
+  height: 1rem;
+  vertical-align: middle;
+  background: green;
+  display: inline-block;
+  border-radius: 50%;
+  text-align: center;
+`
+
+function Best() {
+  return (
+    <Circle css={css`background: green;`}>
+      <Check size={13} />
+    </Circle>
+  )
+}
+
+function Worst() {
+  return (
+    <Circle css={css`background: red;`}>
+      <X size={13} />
+    </Circle>
+  )
+}
+
+
 export default function Comparison() {
   return (
     <Section
@@ -62,6 +135,56 @@ export default function Comparison() {
       `}
     >
       <Heading>Your Options for Microgravity</Heading>
+      <Container>
+      <Table>
+        {/*
+        <thead>
+          <td></td>
+          <td><img src={EiffelTower} /></td>
+          <td><img src={Rocket}/></td>
+          <td><img src={ISS}/></td>
+        </thead>
+         */}
+        <thead>
+          <td></td>
+          <td>Drop Tower</td>
+          <td>Hudson Space</td>
+          <td>ISS Experiment</td>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Microgravity</td>
+            <td>10 sec <Worst/></td>
+            <td>3 min <Best/></td>
+            <td>∞</td>
+          </tr>
+          <tr>
+            <td>Weight Limit</td>
+            <td>264 kg</td>
+            <td>80 kg <Best/></td>
+            <td>4 kg <Worst/></td>
+          </tr>
+          <tr>
+            <td>Iteration</td>
+            <td>rapid</td>
+            <td>rapid <Best/></td>
+            <td>none <Worst/></td>
+          </tr>
+          <tr>
+            <td>Retrieval</td>
+            <td>instant</td>
+            <td>instant <Best/></td>
+            <td>months <Worst/></td>
+          </tr>
+          <tr>
+            <td>Cost</td>
+            <td></td>
+            <td>$9,500/kg <Best/></td>
+            <td>$70,000/kg <Worst/></td>
+          </tr>
+        </tbody>
+      </Table>
+    </Container>
       <Container>
         <Row>
           <Image>
