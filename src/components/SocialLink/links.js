df47f839e49fb233a40d9b411e918007786c043e
Ben Iofel <ben@hudsonspace.systems>
2021-04-25 22:40:10 -0400

    Remove github link and reorder social links

--- src/components/SocialLink/links.js
+++ src/components/SocialLink/links.js
@@ -1,6 +1,5 @@
 import {
   LogoTwitter32,
-  LogoGithub32,
   LogoFacebook32,
   LogoInstagram32,
   LogoLinkedin32,
@@ -8,24 +7,20 @@ import {
 
 const links = [
   {
-    href: 'https://twitter.com/HudsonSpaceSys',
-    icon: LogoTwitter32,
+    href: 'https://www.linkedin.com/company/hudsonspacesys/',
+    icon: LogoLinkedin32,
   },
   {
-    href: 'https://www.facebook.com/hudsonspacesys',
-    icon: LogoFacebook32,
+    href: 'https://twitter.com/HudsonSpaceSys',
+    icon: LogoTwitter32,
   },
   {
     href: 'https://www.instagram.com/hudsonspacesys/',
     icon: LogoInstagram32,
   },
   {
-    href: 'https://www.linkedin.com/company/hudsonspacesys/',
-    icon: LogoLinkedin32,
-  },
-  {
-    href: 'http://github.com/hudsonspace/',
-    icon: LogoGithub32,
+    href: 'https://www.facebook.com/hudsonspacesys',
+    icon: LogoFacebook32,
   },
 ]
 
