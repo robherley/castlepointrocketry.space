d9d793f026e22f4ec2e87203298f2863511aa5ca
Ben Iofel <github@iofel.me>
2021-12-29 23:06:52 -0500

    Add 2021 investor report page

---
+++ src/pages/2021-investor-report.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+import { navigate } from '@reach/router'
+
+const InvestorReport = () => {
+  useEffect(() => {
+    navigate('https://docsend.com/view/kj7yqgt57nbd6igf')
+  }, [])
+  return null
+}
+
+export default InvestorReport
