22f00d7843d8113de6be7c46c7125b05232526cc
Ben Iofel <github@iofel.me>
2022-03-18 16:19:16 -0400

    Add /handout1

---
+++ src/pages/handout1.js
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+import { navigate } from '@reach/router'
+
+const Handout1 = () => {
+  useEffect(() => {
+    navigate('https://docsend.com/view/p6gc6j3cz2vic8zs')
+  }, [])
+  return null
+}
+
+export default Handout1
